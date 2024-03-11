c6a6da12dafee0e48d9033e15ca671a8eb79aae3
Yago Calomino <yagoferrocalomino@gmail.com>
2024-03-11 14:54:43 -0300

    Correção de Lint2

--- app/backend/src/middlewares/LoginValidations.ts
+++ app/backend/src/middlewares/LoginValidations.ts
@@ -1,34 +1,41 @@
 import { Request, Response, NextFunction } from 'express';
 import mapStatusHTTP from '../utils/mapStatusHTTP';
+import JWT from '../utils/JWT';
 
-class LoginValidations {
-  static async validateLogin(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Response | void> {
+export default class AuthValidations {
+  static checkLogin(req: Request, res: Response, next: NextFunction): Response | void {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res
-        .status(mapStatusHTTP.badRequest)
-        .send({ message: 'All fields must be filled' });
+      return res.status(mapStatusHTTP.badRequest).json({ message: 'All fields must be filled' });
     }
 
-    if (!(typeof email === 'string' && typeof password === 'string')) {
-      return res.status(400).send({ message: 'Email and password must be strings' });
+    if (typeof email !== 'string' || typeof password !== 'string') {
+      return res.status(400).json({ message: 'Email and password must be strings' });
     }
 
-    const emailRegex = new RegExp('^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,3})+$');
+    const emailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (password.length < 6 || !emailRegex.test(email)) {
-      return res
-        .status(mapStatusHTTP.unauthorized)
-        .send({ message: 'Invalid email or password' });
+    if (password.length < 6 || !emailPattern.test(email)) {
+      return res.status(mapStatusHTTP.unauthorized).json({ message: 'Invalid email or password' });
     }
 
     next();
   }
-}
 
-export default LoginValidations;
+  static async checkToken(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(mapStatusHTTP.unauthorized).json({ message: 'Token not found' });
+    }
+    const token = authHeader.split(' ')[1];
+    const isValidToken = await JWT.verify(token);
+
+    if (isValidToken === 'Token must be a valid token') {
+      return res.status(mapStatusHTTP.unauthorized).json({ message: isValidToken });
+    }
+    res.locals.payload = isValidToken;
+    next();
+  }
+}
