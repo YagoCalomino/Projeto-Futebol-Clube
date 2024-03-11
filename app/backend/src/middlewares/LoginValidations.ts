c22c216fae96ac033e58432ffbecbfea9f5d97e6
Yago Calomino <yagoferrocalomino@gmail.com>
2024-03-11 15:25:40 -0300

    Ultima correção de EsLint

--- app/backend/src/middlewares/LoginValidations.ts
+++ app/backend/src/middlewares/LoginValidations.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import mapStatusHTTP from '../utils/mapStatusHTTP';
 import JWT from '../utils/JWT';
 
-export default class AuthValidations {
-  static checkLogin(req: Request, res: Response, next: NextFunction): Response | void {
+export default class LoginValidations {
+  static validateLogin(req: Request, res: Response, next: NextFunction): Response | void {
     const { email, password } = req.body;
 
     if (!email || !password) {
@@ -14,28 +14,28 @@ export default class AuthValidations {
       return res.status(400).json({ message: 'Email and password must be strings' });
     }
 
-    const emailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
-    if (password.length < 6 || !emailPattern.test(email)) {
+    if (password.length < 6 || !emailRegex.test(email)) {
       return res.status(mapStatusHTTP.unauthorized).json({ message: 'Invalid email or password' });
     }
 
     next();
   }
 
-  static async checkToken(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers.authorization;
+  static async validateToken(req: Request, res: Response, next: NextFunction) {
+    const bToken = req.headers.authorization;
 
-    if (!authHeader) {
+    if (!bToken) {
       return res.status(mapStatusHTTP.unauthorized).json({ message: 'Token not found' });
     }
-    const token = authHeader.split(' ')[1];
-    const isValidToken = await JWT.verify(token);
+    const token = bToken.split(' ')[1];
+    const validToken = await JWT.verify(token);
 
-    if (isValidToken === 'Token must be a valid token') {
-      return res.status(mapStatusHTTP.unauthorized).json({ message: isValidToken });
+    if (validToken === 'Token must be a valid token') {
+      return res.status(mapStatusHTTP.unauthorized).json({ message: validToken });
     }
-    res.locals.payload = isValidToken;
+    res.locals.payload = validToken;
     next();
   }
 }
