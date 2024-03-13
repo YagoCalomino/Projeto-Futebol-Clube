e947d283976c5b347e2c061f58339337b18fb2b6
Yago Calomino <yagoferrocalomino@gmail.com>
2024-03-13 17:49:06 -0300

    Correção de EsLint

--- app/backend/src/Interfaces/leaderboard/ILeaderboard.ts
+++ app/backend/src/Interfaces/leaderboard/ILeaderboard.ts
@@ -1,10 +1,10 @@
 export interface ILeaderboard {
-    name: string;
-    totalPoints: number;
-    totalGames: number;
-    totalVictories: number;
-    totalDraws: number;
-    totalLosses: number;
-    goalsFavor: number;
-    goalsOwn: number;
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalDraws: number;
+  totalLosses: number;
+  goalsFavor: number;
+  goalsOwn: number;
 }
