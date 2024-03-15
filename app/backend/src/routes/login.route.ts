3d508932cd8942df222243dcd2946e3063fcaac7
Yago Calomino <yagoferrocalomino@gmail.com>
2024-03-14 21:29:32 -0300

    Correção de Lint

--- app/backend/src/routes/login.route.ts
+++ app/backend/src/routes/login.route.ts
@@ -1,24 +1,13 @@
-// Importando as dependências necessárias
 import { Request, Response, Router } from 'express';
 import Login from '../Controller/LoginController';
-import emailPasswordValidations from '../middlewares/emailPasswordValidations';
+import emailPasswordValidations from '../middlewares/emailPasswordValidations'; //Teste de Importação, erro ao puxar
 import tokenValidation from '../middlewares/tokenValidation';
 
-// Inicializando o roteador
 const login = Router();
 
-// Rota para realizar o login
-// "emailPasswordValidations" é um middleware que valida o email e a senha
 login.post('/login', emailPasswordValidations, (req: Request, res: Response) =>
-  // "login" é um método no controlador de login
   Login.login(req, res));
 
-// Rota para obter a função do usuário
-// "tokenValidation" é um middleware que valida o token
-login.get('/login/role', tokenValidation,
+login.get('/login/role', tokenValidation, Login.getRole); // teste de rota, mudar
 
-// "getRole" é um método no controlador de login
-Login.getRole);
-
-// Exportando o roteador
 export default login;
